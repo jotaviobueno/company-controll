c600075b283dcb8897c67bae356aa3b7e7b5017f
jotaviobueno <joseotaviocbueno@gmail.com>
2023-09-06 18:55:11 -0300

    save

--- src/modules/stock/stock.resolver.spec.ts
+++ src/modules/stock/stock.resolver.spec.ts
@@ -1,18 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StockResolver } from './stock.resolver';
 import { stockModuleStock } from './stock.module';
+import { StockCreateUseCase } from './use-cases';
+import { createStockInputMock, stockMock } from 'src/domain/mocks';
 
 describe('StockResolver', () => {
   let resolver: StockResolver;
+  let createUseCase: StockCreateUseCase;
 
   beforeEach(async () => {
     const module: TestingModule =
       await Test.createTestingModule(stockModuleStock).compile();
 
     resolver = module.get<StockResolver>(StockResolver);
+    createUseCase = module.get<StockCreateUseCase>(StockCreateUseCase);
   });
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  it('should create', async () => {
+    jest.spyOn(createUseCase, 'execute').mockResolvedValue(stockMock);
+
+    expect(await resolver.createStock(createStockInputMock)).toStrictEqual(
+      stockMock,
+    );
   });
 });
